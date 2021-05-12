0b39eb49790943c38fc2be4245e47e68127ed801
princejb786 <sam.jbjin@gmail.com>
2021-05-12 21:26:12 +0530

    violet: Add more packages
    Signed-off-by: princejb786 <sam.jbjin@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := NfcNci AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions SprintDM SprintHM VzwOmaTrigger YouTube YouTubeMusicPrebuilt TipsPrebuilt Maps Drive GoogleTTS Music PrebuiltGmail MicropaperPrebuilt NgaResources SafetyHubPrebuilt WallpapersBReel2020 SoundAmplifierPrebuilt GoogleCamera AppDirectedSMSService ConnMO DCMO OemDmTrigger Showcase USCCDM VZWAPNLib libqcomfm_jni obdm_stub Snap 
+LOCAL_OVERRIDES_PACKAGES := NfcNci AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions SprintDM SprintHM VzwOmaTrigger YouTube YouTubeMusicPrebuilt TipsPrebuilt Maps Drive GoogleTTS Music PrebuiltGmail MicropaperPrebuilt NgaResources SafetyHubPrebuilt WallpapersBReel2020 SoundAmplifierPrebuilt GoogleCamera AppDirectedSMSService ConnMO DCMO OemDmTrigger Showcase USCCDM VZWAPNLib libqcomfm_jni obdm_stub Camera2 DevicePolicyPrebuilt arcore LocationHistoryPrebuilt PixelLiveWallpaperPrebuilt PixelWallpapers2020 WallpapersBReel2020 MicropaperPrebuilt talkback GCS  
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
